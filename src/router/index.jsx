46770fe55f4ee01692546c6275df12e99fc3a5ca
jromariombr <Romario@mbr.net.br>
2023-10-10 17:20:48 -0300

    game23 implementado junto com json

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -36,6 +36,7 @@ import { Game19 } from "../components/game19";
 import { Game20 } from "../components/game20";
 import { Game21 } from "../components/game21";
 import { Game22 } from "../components/game22";
+import { Game23 } from "../components/game23";
 
 import { GameSL1 } from "../components/game_sl1";
 import { GameSL2 } from "../components/game_sl2";
@@ -84,6 +85,7 @@ export const Router =() => {
       <Route path = "/Game20" element={<Game20 />}/>
       <Route path = "/Game21" element={<Game21 />}/>
       <Route path = "/Game22" element={<Game22 />}/>
+      <Route path = "/Game23" element={<Game23 />}/>
       <Route path = "/GameSL1" element={<GameSL1 />}/>
       <Route path = "/GameSL2" element={<GameSL2 />}/>
       <Route path = "/GameSL3" element={<GameSL3 />}/>
