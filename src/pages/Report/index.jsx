83f28aa314da83f2d106949165f6a053cc2d9510
jromariombr <Romario@mbr.net.br>
2023-10-25 17:57:38 -0300

    rota report iniciada

--- src/pages/Report/index.jsx
+++ src/pages/Report/index.jsx
@@ -1,9 +1,96 @@
-import { ReportContainer } from "./styles"
+import { ButtonBg } from "../../components/ButtonBg";
+import { LineSeparator } from "../../components/LineSeparator";
+import { TopMenuHeader } from "../../components/TopMenuHeader";
+
+import Robo from "../../assets/avatarRobo.png";
+import PrintImg from "../../assets/print.png";
+
+import { Container, Main, Avatar, AvatarArea, AvatarInfo, Print, XP, Stage, Details, XPLeft, XPRight, XPDiv, XPInside, SLeft, SRight, DDiv } from "./styles";
+import { defaultTheme } from "../../themes/defaultTheme";
 
 export const Report = () => {
   return (
-    <ReportContainer>
-      <p>Report</p>
-    </ReportContainer>
+    <Container>
+      <TopMenuHeader />
+      
+      <div>
+        <AvatarArea>
+          <AvatarInfo>
+            <Avatar>
+              <img src={Robo} alt="" />
+            </Avatar>
+            <p>Carlos Alberto</p>
+          </AvatarInfo>
+          <Print>
+            <img src={PrintImg} alt="" />
+            <p>Print Report</p>
+          </Print>
+        </AvatarArea>
+      </div>
+
+      <Main>
+        <XP>
+          <LineSeparator w="18.5rem" bg={defaultTheme["gray-200"]} mt="0" mb="0" />
+          <XPDiv>
+            <XPLeft>
+              <XPInside>
+                <span>XP:</span>
+                <p>XXX</p>
+              </XPInside>
+              <XPInside>
+                <span>FISK DOLLARS:</span>
+                <p>XXX</p>
+              </XPInside>
+            </XPLeft>
+            <XPRight><p>Access</p></XPRight>
+          </XPDiv>
+          <LineSeparator w="18.5rem" bg={defaultTheme["gray-200"]} mt="0" mb="0" />
+        </XP>
+        <Stage>
+          <SLeft>
+            <h2>Stage progress:</h2>
+
+            <div></div>
+          </SLeft>
+          <SRight>
+            <span>80%</span>
+            <p>of lessons completed</p>
+          </SRight>
+        </Stage>
+        <Details>
+          <LineSeparator w="18.5rem" bg={defaultTheme["gray-200"]} mt="0" mb="0" />
+          <DDiv>
+            <div></div>
+            <div>
+              <h2>Listening</h2>
+
+              <ul>
+                <li>70% success rate</li>
+                <li>100% of activities completed</li>
+                <li>21 correct answers out of 30</li>
+                <li>00:00</li>
+              </ul>
+            </div>
+          </DDiv>
+          <LineSeparator w="18.5rem" bg={defaultTheme["gray-200"]} mt="0" mb="0" />
+          <DDiv>
+            <div></div>
+            <div>
+              <h2>Listening</h2>
+
+              <ul>
+                <li>70% success rate</li>
+                <li>100% of activities completed</li>
+                <li>21 correct answers out of 30</li>
+                <li>00:00</li>
+              </ul>
+            </div>
+          </DDiv>
+          <LineSeparator w="18.5rem" bg={defaultTheme["gray-200"]} mt="0" mb="0" />
+        </Details>
+
+        <ButtonBg title="Home" w="15.875rem" h="2.5rem" />
+      </Main>
+    </Container>
   )
 }
--- src/pages/Report/styles.jsx
+++ src/pages/Report/styles.jsx
@@ -1,3 +1,177 @@
 import styled from "styled-components";
 
-export const ReportContainer = styled.div``;
+export const Container = styled.div`
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Main = styled.main`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const AvatarArea = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 1rem 2rem;
+`;
+
+export const AvatarInfo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1.5rem;
+
+  p {
+    font-weight: bold;
+    font-size: 1.25rem;
+  }
+`;
+
+export const Avatar = styled.div`
+  width: 3rem;
+  height: 3rem;
+  border: 1px solid ${props => props.theme["red-200"]};
+  border-radius: 6px;
+  box-shadow: 0px 4px 10px 0px #00000033;
+  cursor: pointer;
+
+  img {
+    width: 100%;
+  }
+`;
+
+export const Print = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 0.125rem;
+  width: 3rem;
+  height: 3rem;
+  box-shadow: 0px 4px 10px 0px #00000033;
+  border-radius: 6px;
+  cursor: pointer;
+
+  p {
+    font-size: 0.675rem;
+    text-align: center;
+  }
+
+  img {
+    width: 50%;
+  }
+`;
+
+export const XP = styled.div`
+  width: 100%;
+  padding-inline: 2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  gap: 1rem;
+  background-color: ${props => props.theme["gray-100"]};
+`;
+
+export const XPDiv = styled.div`
+  width: 18.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const XPLeft = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  gap: 0.25rem;
+`;
+
+export const XPInside = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+
+  span {
+    color: ${props => props.theme["red-200"]};
+  }
+
+  p {
+    color: ${props => props.theme["gray-700"]};
+    font-weight: 500;
+  }
+`;
+
+export const XPRight = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  p {
+    color: ${props => props.theme["blue-100"]};
+    font-size: 1.25rem;
+    font-weight: 500;
+    text-decoration: underline;
+    cursor: pointer;
+  }
+`;
+
+export const Stage = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 2rem 2rem;
+  gap: 0.5rem;
+`;
+
+export const SLeft = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+  h2 {
+    font-size: 1rem;
+  }
+
+  div {
+    width: 80px;
+    height:80px;
+    border-radius: 9999px;
+    background-color: red;
+  }
+`;
+
+export const SRight = styled.div`
+  flex: 1;
+  text-align: center;
+  span {
+    color: ${props => props.theme["red-200"]};
+  }
+
+  p {
+    color: ${props => props.theme["gray-700"]};
+    font-weight: 500;
+  }
+`;
+
+export const Details = styled.div`
+  width: 100%;
+  padding-inline: 2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  gap: 1rem;
+  background-color: ${props => props.theme["gray-100"]};
+`;
+
+export const DDiv = styled.div``;
